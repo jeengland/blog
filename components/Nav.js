65033c56149d56f203eaf9e664fd2da9625973d6
unknown <jacob.e.england@gmail.com>
2020-02-28 00:09:13 -0600

    Added conditional navbar highlighting logic

--- components/Nav.js
+++ components/Nav.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 
 const Nav = () => {
-    return (
-        <nav>
-            {/* #TODO add a logo */}
-            <NavLink className='current-page' to='/' onClick={() => window.scrollTo(0, 0)}>Home</NavLink>
-            <a href='#'>About</a>
-            <a href='https://jacobengland.computer/nasa-photo-of-the-day'></a>
-        </nav>
-    )
+    const page = useHistory().location.pathname;
+    if (page === '/') {
+        return (
+            <nav>
+                {/* #TODO add a logo */}
+                <NavLink className='current-page' to='/' onClick={() => window.scrollTo(0, 0)}>Home</NavLink>
+                <a href='#'>About</a>
+                <a href='https://jacobengland.computer/nasa-photo-of-the-day'></a>
+            </nav>
+        )
+    } else {
+        return (
+            <nav>
+                {/* #TODO add a logo */}
+                <NavLink to='/' onClick={() => window.scrollTo(0, 0)}>Home</NavLink>
+                <a href='#'>About</a>
+                <a href='https://jacobengland.computer/nasa-photo-of-the-day'></a>
+            </nav>
+        )
+    }
 }
 
 export default Nav;
