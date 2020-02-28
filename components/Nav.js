c43d2abe2e8cf6989409a536dd3bf46a2f075dff
unknown <jacob.e.england@gmail.com>
2020-02-27 23:35:40 -0600

    Fixed nav links to route properly

--- components/Nav.js
+++ components/Nav.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
     return (
         <nav>
             {/* #TODO add a logo */}
-            <a className='current-page' href='#'>Home</a>
-            <a href='about.html'>About</a>
+            <NavLink className='current-page' to='/'>Home</NavLink>
+            <a href='#'>About</a>
             <a href='https://jacobengland.computer/nasa-photo-of-the-day'></a>
         </nav>
     )
